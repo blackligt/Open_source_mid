d809087334bb4ca2d3a1e0caadbd373916d1df86
Cheon <cheonsa30@naver.com>
2022-11-05 19:06:34 +0900

    calcutaed function to other file

---
+++ function.py
@@ -0,0 +1,27 @@
+# This function adds two numbers
+def add(add_num1, add_num2):
+    '''
+    두 수를 입력받고 더하는 함수
+    '''
+    return add_num1 + add_num2
+
+# This function subtracts two numbers
+def subtract(sub_num1, sub_num2):
+    '''
+    두 수를 입력받고 빼는 함수
+    '''
+    return sub_num1 - sub_num2
+
+# This function multiplies two numbers
+def multiply(mul_num1, mul_num2):
+    '''
+    두 수를 입력받고 곱하는 함수
+    '''
+    return mul_num1 * mul_num2
+
+# Need to define divide function.
+def divide(div_num1, div_num2):
+    '''
+    두 수를 입력받고 나누는 함수
+    '''
+    return div_num1/div_num2
